4ebcb3d18b5010335a1e5ceb878ba4335d86f14a
NishaDevi07 <120001149+NishaDevi07@users.noreply.github.com>
2023-03-14 21:45:07 +0530

    Add files via upload

---
+++ Hibernate1/src/main/java/com/BusInfo/Hibernate1/App.java
@@ -0,0 +1,14 @@
+package com.BusInfo.Hibernate1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    
+    public static void main( String[] args ){
+		BusManagement busobj = new BusManagement();
+		busobj.toReadInput();
+	}
+}
